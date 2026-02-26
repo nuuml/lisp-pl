73afa921ab305ca7175433493d4baeacd0a3ca1c
nuuml <126520244+nuuml@users.noreply.github.com>
2026-02-26 15:10:57 -0600

    lambda because i can

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use input_utils::get_block::get_inner_block;
 
 fn main() {
     println!("Welcome to LISP repl");
-    println!("Please enter command:");
-    let mut command = String::new();
-    let stdin = std::io::stdin();
-    stdin.read_line(&mut command).unwrap();
-    println!("Command: {}", get_inner_block(command)[0]);
+    loop {
+        eprint!(asdf"𝜆:");
+        let mut command = String::new();
+        let stdin = std::io::stdin();
+        stdin.read_line(&mut command).unwrap();
+        println!("Command: {}", get_inner_block(command)[0]);
+    }
 }
