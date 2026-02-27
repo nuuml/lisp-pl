48499aebeffaeb8a80b85e30a840a099e8adffde
nuuml <126520244+nuuml@users.noreply.github.com>
2026-02-27 16:39:21 -0600

    small update. will do more choomorrow

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use input_utils::get_block::get_inner_block;
 fn main() {
     println!("Welcome to LISP repl");
     loop {
-        eprint!(asdf"𝜆:");
+        eprint!("𝜆:");
         let mut command = String::new();
         let stdin = std::io::stdin();
         stdin.read_line(&mut command).unwrap();
