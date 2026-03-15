05b494b9cb6ac516f0b413907e30a5e0a7ae7041
nuuml <126520244+nuuml@users.noreply.github.com>
2026-03-15 13:29:54 -0500

    scaffolding some list functions

--- src/input_utils/run_command/ops/list.rs
+++ src/input_utils/run_command/ops/list.rs
@@ -3,12 +3,16 @@ use crate::input_utils::run_command::value::LispValue;
 #[derive(Debug, Clone, PartialEq)]
 pub enum ListOp {
     List,
+    Cdr,
+    Car
 }
 
 impl ListOp {
     pub fn from_str(s: &str) -> Option<Self> {
         match s {
             "list" => Some(Self::List),
+            "cdr" => Some(Self::Cdr),
+            "car" => Some(Self::Car),
             _ => None,
         }
     }
@@ -17,5 +21,7 @@ impl ListOp {
 pub fn eval_list_op(op: &ListOp, args: &[LispValue]) -> Result<LispValue, String> {
     match op {
         ListOp::List => Ok(LispValue::List(args.to_vec())),
+        ListOp::Cdr => Ok(LispValue::List(args.to_vec())),
+        ListOp::Car => Ok(LispValue::List(args[0..0].to_vec())),
     }
 }
